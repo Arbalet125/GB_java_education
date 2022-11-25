9be3d3c459734d2dca9ab41a3cfd95db2ad6cb88
Arbalet <MametovV@gmail.com>
2022-11-25 22:27:36 +0300

    end 2-3 task

--- second_seminar/dz_2_seminar/main.java
+++ second_seminar/dz_2_seminar/main.java
@@ -22,3 +22,49 @@ public class main {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////Второе задание копирует первое, так что повторять не вижу смысла/////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Напишите программу для объединения данной строки с самим собой заданное/////////////////////////
+//количество раз(количество и строку мы вводим)//////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+package GB_java_education.second_seminar.dz_2_seminar;
+import java.util.Scanner;
+import java.util.Arrays;
+public class ьain {    
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in); 
+        System.out.println("Введите исходную строку:");
+        String var = iScanner.nextLine();
+        System.out.println("Исходная строка: " + var);
+        System.out.println("Введите количество объеденений строки:");
+        int n = Integer.parseInt(iScanner.nextLine());
+        String result = repeat_str(var, n);
+        System.out.println("После повторения" + n + "раз, результат: " + result);
+        iScanner.close();
+   }
+public static String repeat_str(String var, int n) {
+       if (var == null || var.isEmpty()) {
+           return "";
+       }
+       if (n <= 0) {
+           return var;
+       }
+       if (Integer.MAX_VALUE / n < var.length()) {
+           throw new OutOfMemoryError("Слишком большое число, введите другое");
+       }
+       StringBuilder x = new StringBuilder(var.length() * n);
+       for (int i = 1; i <= n; i++) {
+           x.append(var);
+       }
+       return x.toString();
+   }
+} */
+
+
