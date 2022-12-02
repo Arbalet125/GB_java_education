57cb741bb89af9dcde303200aa699da25ffb6a1c
Arbalet <MametovV@gmail.com>
2022-12-02 20:29:04 +0300

    end task

--- fifth_seminar/dz_5_seminar/main.java
+++ fifth_seminar/dz_5_seminar/main.java
@@ -1,5 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Дана строка. Необходимо написать метод, который отсортирует слова в строке по длине с помощью TreeMap.
+// Слова с одинаковой длиной не должны “потеряться”.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 
 package GB_java_education.fifth_seminar.dz_5_seminar;
 
-public class main {
-    
-}
+import java.util.*;
+import java.util.Map.Entry;
+import java.io.*;
+  
+public class GFG {
+  
+    static String[] sort(String[] str, int n)
+    {
+        TreeMap<Integer, ArrayList<String>> map = new TreeMap<Integer, ArrayList<String> >();
+  
+        for (int i = 0; i < n; i++) {
+  
+            map.putIfAbsent(str[i].length(), new ArrayList<String>());
+            map.get(str[i].length()).add(str[i]);
+        }
+  
+        int temp = 0;
+  
+        for (Entry<Integer, ArrayList<String> >
+                 e : map.entrySet()) {
+  
+            for (int i = 0; i < e.getValue().size(); i++) {
+  
+                str[temp] = e.getValue().get(i);
+                temp++;
+            }
+        }
+        return str;
+    }
+  
+    static void printArraystring(String str[], int n)
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(str[i] + " ");
+    }
+  
+    public static void main(String args[])
+    {
+        String[] arr = {"Если", "вы", "можете", "мечтать", "об", "этом", "вы", "можете", "это", "сделать"};
+        int n = arr.length;
+  
+        arr = sort(arr, n);
+  
+        printArraystring(arr, n);
+    }
+} */
